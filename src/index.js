811654ef3786299d44317459479dfeef7aa505f2
Chapchik <88148518+Chapchik@users.noreply.github.com>
2021-12-21 16:18:27 +0200

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let abs = Math.abs(n);
+  let res = '';
+  let digit;
+
+  while (abs) {
+    digit = abs % 10;
+    res = res + digit;
+    abs = Math.floor(abs / 10);
+  }
+  return Number(res);
 }
